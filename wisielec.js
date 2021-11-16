5a9d494ec99ecea887b353516ecbad13499ca20e
Michal-Piotrkowski <michal.piotrkowski@protonmail.com>
2021-11-16 18:50:53 +0100

    Zmiany

--- wisielec.js
+++ wisielec.js
@@ -5,6 +5,11 @@ let game = {
     n: 17,
     m: 2,
     mistakes_counter: 9,
+    all_letters: 0,
+    blank: 0,
+    start_time: 0,
+    endTime: 0,
+    isTime: false,
     letters: ["A", "Ą", "B", "C", "Ć", "D", "E", "Ę", "F", "G", "H", "I", "J", "K", "L", "Ł", "M", "N", "Ń", "O", "Ó", "P", "Q", "R", "S", "Ś", "T", "U", "V", "W", "X", "Y", "Z", "Ż"],
     riddles: ["FLYING OCTOPUS", "DOM LESTERÓW", "WOJOWNICZY MYSZOJELEŃ", "STWÓRZ BOHATERA", "MAGICAL FIGHTER", "DISCORD", "UNITY", "OŚMIORNICA", "GRY TO NASZA PASJA", "ASTERIELLE SLAYER OF DWARVES"],
     opis: "rozpoczynamy gre",
@@ -18,6 +23,8 @@ let game = {
         src.appendChild(img);
         console.log(this.letters.length)
         this.make_board();
+        this.isTime = true
+        this.start_time = new Date()
     },
     make_board: function () {
         for (let i = 0; i < this.m; i++) {
@@ -27,6 +34,7 @@ let game = {
                 div.classList.add("pole")
                 document.getElementById("board").appendChild(div)
                 if (this.in_game != false) {
+                    this.all_letters = this.answer.length
                     div.addEventListener("click", (e) => { this.zd(this, e.target) });
                 }
                 this.arr[i][j] = 0
@@ -56,32 +64,51 @@ let game = {
             document.getElementById("sign").appendChild(div2)
             this.answer[i] = sign_letters_arr[i]
             if (this.answer[i] == " ") {
+                this.blank += 1
                 div2.innerHTML = ""
-                div2.style.backgroundColor = "white"
+                div2.style.background = "transparent"
                 div2.style.border = "none"
             }
         }
     },
 
     zd: function (d, e, arr) {
+        if (this.in_game == false) {
+            return
+        }
         if (this.mistakes_counter > 1) {
             let is_okay = false
             for (let i = 0; i < this.answer.length; i++) {
                 if (this.answer[i] == e.innerHTML && document.getElementById(i + "2").innerHTML != e.innerHTML) {
                     document.getElementById(i + "2").innerHTML = e.innerHTML
+                    this.all_letters -= 1
+                    if (this.all_letters - this.blank == -this.answer.length) {
+                        document.getElementById("end").innerHTML = "WINNER";
+                        document.getElementById("end").style.visibility = "visible";
+                        gsap.to("#end", { duration: 2, x: "50vw", opacity: 1 });
+                        clearInterval(x)
+                        document.getElementById("timer").innerHTML = "OCTOPUS"
+                        this.in_game = false
+                        this.isTime = false
+                    }
+                    this.start_time = new Date()
+                    console.log(this.start_time)
                     is_okay = true
                 }
             }
             if (is_okay == false) {
                 document.getElementById("img").src = "img/img" + (this.mistakes_counter - 1) + ".png";
                 this.mistakes_counter -= 1
-                console.log(this.mistakes_counter)
+                if (this.mistakes_counter == 1) {
+                    this.in_game = false;
+                    setTimeout(() => document.getElementById("img").src = "img/img" + (this.mistakes_counter - 1) + ".png", 500)
+                    document.getElementById("end").innerHTML = "GAME OVER";
+                    document.getElementById("end").style.visibility = "visible";
+                    gsap.to("#end", { duration: 2, x: "50vw", opacity: 1 });
+                    this.isTime = false
+                    this.endTime = new Date()
+                }
             }
-            console.log("git")
-        }
-        else {
-            this.in_game = false;
-            alert("PRZEGRAŁEŚ")
         }
     },
     signGenerator: function () {
@@ -89,3 +116,27 @@ let game = {
         return sign_number
     },
 }
+
+let x = setInterval((e) => {
+    let time
+    if (game.isTime) {
+        time = new Date() - game.start_time
+    }
+    else {
+        time = game.endTime - game.start_time
+    }
+
+    if (time > 7000) {
+        game.in_game = false;
+        setTimeout(() => document.getElementById("img").src = "img/img0.png", 500)
+        document.getElementById("end").innerHTML = "GAME OVER";
+        document.getElementById("end").style.visibility = "visible";
+        gsap.to("#end", { duration: 2, x: "50vw", opacity: 1 });
+        game.isTime = false
+        game.endTime = new Date()
+        document.getElementById("timer").innerHTML = "KONIEC CZASU"
+    }
+    else {
+        document.getElementById("timer").innerHTML = time / 1000 + "s"
+    }
+}, 1)
